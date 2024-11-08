bf45944c910c14052a9720e526bc9e6db04b485d
Saurav Thakur <sauravthakur1341@gmail.com>
2024-11-08 14:28:43 +0000

    fix:test cases fixed

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,10 @@ from app import app
 
 
 client = TestClient(app=app)
+prefix = "/api/v1/pdf-qa"
 
 
 def test_endpoint():
-    response = client.get("/")
+    response = client.get(f"{prefix}/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
