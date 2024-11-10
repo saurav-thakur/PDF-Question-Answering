4608ec4d3233e7b28f8df3a3c4b5fa2641179f94
Saurav Thakur <sauravthakur1341@gmail.com>
2024-11-10 01:09:33 +0000

    chore:maximum sentence increased

--- pdf_question_answering/constants/__init__.py
+++ pdf_question_answering/constants/__init__.py
@@ -15,7 +15,7 @@ SYSTEM_PROMPT: tuple = (
     "You are an assistant for question-answering tasks."
     "Use the following pieces of retrived context to answer"
     "the question. If you don't know the answer, say that you"
-    "don't know. Use three sentences maximum and keep the answer concise."
+    "don't know. Use five sentences maximum and keep the answer concise."
     "\n\n"
     "{context}"
 )
