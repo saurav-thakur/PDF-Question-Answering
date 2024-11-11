8ab57af380d974ab9ad857d00c4fb0743bf788cf
Saurav Thakur <sauravthakur1341@gmail.com>
2024-11-11 13:03:17 +0000

    chore:long description removed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_desc = f.read()
+# with open("README.md", "r", encoding="utf-8") as f:
+#     long_desc = f.read()
 
 setuptools.setup(
     name="pdf_question_answering",
@@ -9,6 +9,6 @@ setuptools.setup(
     author="Saurav",
     author_email="thakursaurav1341@gmail.com",
     description="pdf question answer chat application",
-    long_description=long_desc,
+    # long_description=long_desc,
     packages=setuptools.find_packages(),
 )
